3b8d4943a20d12f3ae635577cb11fad1321176b7
Marcelo Sena <marcelomarcos2@gmail.com>
2022-05-31 14:51:57 -0300

    User entity and resource

---
+++ src/main/java/com/devsena/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.devsena.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsena.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Marcelo", "marcelomarcos2@gmail.com", "9999", "111");
+		return ResponseEntity.ok().body(u);
+			
+	}
+
+}
